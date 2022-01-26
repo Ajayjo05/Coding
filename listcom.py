df7059dc5a231dfa3b3f8a7b83b1c8ffde263393
ajayjo05 <ajay.joshi155@gmail.com>
2022-01-26 17:19:37 +0530

    first time

---
+++ listcom.py
@@ -0,0 +1,2 @@
+a = list(map(lambda x: x ** 2, range(10)))
+print(a)
---
+++ main.py
@@ -0,0 +1,11 @@
+print("to print even numbers")
+for i in range(11):
+    if i%2==0:
+        print (i)
+    else:
+        pass
+
+
+a=[x for x in range(10) if x%2==0]
+print(a)
+
